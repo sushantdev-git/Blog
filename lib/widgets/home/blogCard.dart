c0bc46ead698bad93ee575fd2b0dd6bc0e515f8e
lovetoz1 <lovetozpoonia@gmail.com>
2022-03-19 23:23:32 +0530

    added firestore

--- lib/widgets/home/blogCard.dart
+++ lib/widgets/home/blogCard.dart
@@ -2,36 +2,49 @@ import 'package:flutter/material.dart';
 import 'package:home/pages/blogpage.dart';
 
 class BlogCard extends StatelessWidget {
-  const BlogCard({Key? key}) : super(key: key);
+  BlogCard({this.likes, this.image, this.Title, this.author});
+  int? likes;
+  String? image;
+  String? Title;
+  String? author;
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: GestureDetector(
-        onTap: () => Navigator.of(context).push(MaterialPageRoute(builder: (context) => const BlogPage())),
-        child: Card(
+        child: GestureDetector(
+      onTap: () => Navigator.of(context)
+          .push(MaterialPageRoute(builder: (context) => const BlogPage())),
+      child: Card(
         margin: const EdgeInsets.symmetric(vertical: 4, horizontal: 10),
         child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          Image.network(
-          'https://docs.flutter.dev/assets/images/dash/dash-fainting.gif'),
-          ListTile(
-          contentPadding: EdgeInsets.symmetric(vertical: 5, horizontal: 7),
-          leading: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              IconButton(onPressed: (){
-              }, icon: const Icon(Icons.favorite_border, color: Colors.redAccent,),),
-              Text("200", style: TextStyle(fontSize: 14),)
-            ],
-          ),
-          title: Text('The Enchanted Nightingale'),
-          subtitle: Text('Music by Julie Gable. Lyrics by Sidney Stein.'),
-          ),
-        ],
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Image.network(
+                'https://docs.flutter.dev/assets/images/dash/dash-fainting.gif'),
+            ListTile(
+              contentPadding: EdgeInsets.symmetric(vertical: 5, horizontal: 7),
+              leading: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.favorite_border,
+                      color: Colors.redAccent,
+                    ),
+                  ),
+                  Text(
+                    "${likes}",
+                    style: TextStyle(fontSize: 14),
+                  )
+                ],
+              ),
+              title: Text(Title!),
+              subtitle: Text(author!),
+            ),
+          ],
         ),
-        ),
+      ),
     ));
   }
 }
